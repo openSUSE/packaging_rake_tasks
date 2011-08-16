b830d947212a9ef8b490b128fe7a65c77440e914
Josef Reidinger <jreidinger@suse.cz>
2011-08-16 14:01:38 +0200

    first adapted task

--- lib/tasks/packaging_tasks.rb
+++ lib/tasks/packaging_tasks.rb
@@ -1,6 +1,4 @@
 #--
-# Webyast Webservice framework
-#
 # Copyright (C) 2009, 2010 Novell, Inc. 
 #   This library is free software; you can redistribute it and/or modify
 # it only under the terms of version 2.1 of the GNU Lesser General Public
@@ -19,42 +17,30 @@
 require 'rake'
 
 
-class WebserviceTasks
+class PackagingTasks
 
     # a flag - load the tasks just once, multiple loading
     # leads to multiple invocation of the same task
     @@tasks_loaded = false
 
     # load webservice *.rake files, exclude/include list can be specified
-    def WebserviceTasks.loadTasks(params = {:include => ["*.rake"], :exclude => []})
+    def self.loadTasks(params = {:include => ["*.rake"], :exclude => []})
 	return if @@tasks_loaded
+        params[:exclude] ||= []
+	params[:include] ||= ["*.rake"]
+        filelist = {}
 
-	exclude_list = []
-	if params[:exclude].nil?
-	    params[:exclude] = []
-	end
-
-	# expand exclude files
-	params[:exclude].each { |efile| exclude_list += Dir["#{File.dirname(__FILE__)}/#{efile}"]}
-
-	include_list = []
-	if params[:include].nil?
-	    params[:include] = ["*.rake"]
-	end
-
-	# expand include files
-	params[:include].each { |ifile| include_list += Dir["#{File.dirname(__FILE__)}/#{ifile}"]}
+        [:exclude,:include].each do |key|
+          list[key] = params[key].reduce([]) {|ret,file| ret += Dir["#{File.dirname(__FILE__)}/#{file}"] }
+        end
 
 	# load an include file only if it not in the exclude list
-	include_list.each { |ext|
-	    if !exclude_list.include?(ext)
-		load ext
-	    end
-	}
+	filelist[:include].each do |ext|
+	  load ext unless filelist[:exclude].include?(ext)
+	end
 
 	@@tasks_loaded = true
     end
-
 end
 
 # vim: ft=ruby
